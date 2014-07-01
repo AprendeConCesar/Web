c5201681e3c951295e78d11f0eb4fdd3ff4f475a
cesar <cesa>
2014-07-01 18:02:31 -0400

    todavia dandole forma y maketando

---
+++ index2.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Aprende Con Cesar</title>
+	   <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="css/style.css" rel="stylesheet">
+    
+</head>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+<body>
+<div class="container-fluid">
+<div class="row">
+  <div class="col-md-5">
+  	<h1>
+  		Aprende con cesar
+  	</h1>
+  </div>
+
+</div>
+<div class="row">
+<div class="col-md-6"></div>
+<div class="col-md-6">
+	<nav>
+	<ul class="nav nav-pills">
+	  <li><a href="#">Home</a></li>
+	  <li><a href="#">Cursos</a></li>
+	  <li><a href="#">Blog</a></li>
+	  <li><a href="#">Foro</a></li>
+	  <li><a href="#">Contacto</a></li>
+	  <li><a href="#">Acerca</a></li>
+	  <li><a href="#">Login</a></li>
+	</ul>
+	</nav>		
+</div>
+</div>
+
+</div>
+ <footer>
+ <div class="container-fluid">
+ <div class="rou">
+ 	<div class="col-md-6">AprendeConCesar.com </div>
+ 	<div class="col-md-6">
+ 		CONTÁCTANOS
+@AprendeConCesar
+fb.com/AprendeConCesar
+ 	</div>
+
+ </div>
+	
+<div class="rou" id="fin">
+@CesarDarinel
+</div>
+
+
+</div>
+ </footer>
+</body>
+</html>
